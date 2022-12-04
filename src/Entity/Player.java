f2e8026ef29d69495280360a41e5dc1f010aa256
abi-gymnastiar <abiansyahadzanigymnas1012@gmail.com>
2022-12-05 00:49:27 +0700

    Added simple movement, sprites, and simple movement animation

---
+++ src/Entity/Player.java
@@ -0,0 +1,125 @@
+package Entity;
+
+import Main.GamePanel;
+import Main.KeyInputHandler;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+public class Player extends Entity{
+
+    GamePanel gp;
+    KeyInputHandler keyH;
+
+    public Player(GamePanel gp, KeyInputHandler keyH)
+    {
+        this.gp = gp;
+        this.keyH = keyH;
+
+        setDefaultValues();
+        getPlayerImage();
+    }
+    public void setDefaultValues()
+    {
+        x = 100;
+        y = 100;
+        speed = 4;
+        direction = "down";
+    }
+
+    public void getPlayerImage()
+    {
+        try {
+            up1 = ImageIO.read(getClass().getResourceAsStream("/Assets/Player/player_up_01.png"));
+            up2 = ImageIO.read(getClass().getResourceAsStream("/Assets/Player/player_up_02.png"));
+            down1 = ImageIO.read(getClass().getResourceAsStream("/Assets/Player/player_down_01.png"));
+            down2 = ImageIO.read(getClass().getResourceAsStream("/Assets/Player/player_down_02.png"));
+            left1 = ImageIO.read(getClass().getResourceAsStream("/Assets/Player/player_left_01.png"));
+            left2 = ImageIO.read(getClass().getResourceAsStream("/Assets/Player/player_left_02.png"));
+            right1 = ImageIO.read(getClass().getResourceAsStream("/Assets/Player/player_right_01.png"));
+            right2 = ImageIO.read(getClass().getResourceAsStream("/Assets/Player/player_right_02.png"));
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void update()
+    {
+        if(keyH.upPressed == true)
+        {
+            direction = "up";
+            y -= speed;
+        }
+        else if(keyH.downPressed == true)
+        {
+            direction = "down";
+            y += speed;
+        }
+        else if(keyH.leftPressed == true)
+        {
+            direction = "left";
+            x -= speed;
+        }
+        else if(keyH.rightPressed == true)
+        {
+            direction = "right";
+            x += speed;
+        }
+
+        spriteCounter++;
+        if(spriteCounter > 30)
+        {
+            if(spriteNum == 1)
+                spriteNum = 2;
+            else if(spriteNum == 2)
+                spriteNum = 1;
+            spriteCounter = 10;
+        }
+    }
+    public void draw(Graphics2D g2)
+    {
+//        g2.setColor(Color.white);
+//        g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+
+        BufferedImage image = null;
+
+        switch (direction){
+            case "up":
+                if(spriteNum == 1){
+                    image = up1;
+                }
+                if(spriteNum == 2){
+                    image = up2;
+                }
+                break;
+            case "down":
+                if(spriteNum == 1){
+                    image = down1;
+                }
+                if(spriteNum == 2){
+                    image = down2;
+                }
+                break;
+            case "left":
+                if(spriteNum == 1){
+                    image = left1;
+                }
+                if(spriteNum == 2){
+                    image = left2;
+                }
+                break;
+            case "right":
+                if(spriteNum == 1){
+                    image = right1;
+                }
+                if(spriteNum == 2){
+                    image = right2;
+                }
+                break;
+        }
+        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
+    }
+
+}
--- src/Main/GamePanel.java
+++ src/Main/GamePanel.java
@@ -1,8 +1,9 @@
 package Main;
 
+import Entity.Player;
+
 import javax.swing.JPanel;
-import java.awt.Color;
-import java.awt.Dimension;
+import java.awt.*;
 
 public class GamePanel extends JPanel implements Runnable{
 
@@ -10,27 +11,82 @@ public class GamePanel extends JPanel implements Runnable{
     final int originalTileSize = 16;
     final int tileScaling = 3;
 
-    final int tileSize = originalTileSize * tileScaling;
+    public final int tileSize = originalTileSize * tileScaling;
     final int maxScreenColumn = 16, maxScreenRow = 12;
     final int screenWidth = tileSize * maxScreenColumn;
     final int screenHeight = tileSize * maxScreenRow;
 
+    //FPS
+    final int fps = 60;
+
+    KeyInputHandler keyH = new KeyInputHandler();
     Thread gameThread;
+    Player player1 = new Player(this, keyH);
+
+    // Setting the player's initial position
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
 
     public GamePanel()
     {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground((Color.black));
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
     }
 
     public void startGameThread()
     {
         gameThread = new Thread(this);
+        gameThread.start();
     }
 
     @Override
     public void run() {
 
+        double drawInterval = 1000000000/fps;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        long timer = 0;
+        int drawCount = 0;
+
+        while(gameThread != null)
+        {
+            currentTime = System.nanoTime();
+
+            delta += (currentTime - lastTime) / drawInterval;
+            timer += (currentTime - lastTime);
+            lastTime = currentTime;
+
+            if (delta >= 1)
+            {
+                //UPDATE
+                update();
+                //DRAW SCREEN
+                repaint();
+
+                delta--;
+                drawCount++;
+            }
+        }
     }
+
+    public void update()
+    {
+        player1.update();
+    }
+
+    public void paintComponent(Graphics g)
+    {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D)g;
+
+        player1.draw(g2);
+
+        g2.dispose();
+    }
+
 }
